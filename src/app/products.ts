f668475bc941ab5ee0a62f2046a3b8f1effc3ab7
mashabim11 <m_bimurzaeva@kbtu.kz>
2021-02-24 14:22:39 +0600

    4 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,21 +1,117 @@
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    img: "https://m.media-amazon.com/images/I/71aPOVjcHpL._AC_UL480_QL65_.jpg",
+    gal1: "https://m.media-amazon.com/images/I/71aPOVjcHpL._AC_UL480_QL65_.jpg",
+    gal2: "https://images-na.ssl-images-amazon.com/images/I/91rRYi1mHGL._SL1500_.jpg",
+    gal3: "https://images-na.ssl-images-amazon.com/images/I/81zjROIcmXL._SL1500_.jpg",
+    name: 'Maybelline Sky High Washable Mascara',
+    price: 12.5,
+    rating:4.5,
+    description: 'Sky high lash impact from every angle, full volume meets limitless length'
   },
   {
     id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    name: 'Olaplex Hair Perfector',
+    img:"https://m.media-amazon.com/images/I/71gnxxwxAXL._AC_UL480_QL65_.jpg",
+    gal1:"https://m.media-amazon.com/images/I/71gnxxwxAXL._AC_UL480_QL65_.jpg",
+    gal2:"https://images-na.ssl-images-amazon.com/images/I/71oTGRpW-fL._SL1500_.jpg",
+    gal3:"https://images-na.ssl-images-amazon.com/images/I/81F5mznpB8L._SL1500_.jpg",
+    price: 28,
+    rating:4.7,
+    description: 'OLAPLEX No.3 at home hair repairing treatment. '
   },
   {
     id: 3,
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    name: 'Dove Body Wash',
+    img:"https://m.media-amazon.com/images/I/71NbfFpRvzL._AC_UL480_QL65_.jpg",
+    gal1:"https://m.media-amazon.com/images/I/71NbfFpRvzL._AC_UL480_QL65_.jpg",
+    gal2: "https://images-na.ssl-images-amazon.com/images/I/81JFz0S791L._SL1500_.jpg",
+    gal3: "https://images-na.ssl-images-amazon.com/images/I/81c7wVTytuL._SL1500_.jpg",
+    price: 12.09,
+    rating:4.0,
+    description: 'Dove Deep Moisture Body Wash is Just As Effective for Cleaning Hands!'
+  },
+
+  {
+    id: 4,
+    name: 'Makeup Remover ',
+    img:"https://m.media-amazon.com/images/I/81z8tQe35hL._AC_UL480_QL65_.jpg",
+    gal1:"https://m.media-amazon.com/images/I/81z8tQe35hL._AC_UL480_QL65_.jpg",
+    gal2:"https://images-na.ssl-images-amazon.com/images/I/81ZimLGwwaL._SL1500_.jpg",
+    gal3:"https://images-na.ssl-images-amazon.com/images/I/71i0OWKabvL._SL1500_.jpg",
+    price: 11.96,
+    rating:4.5,
+    description: 'Twin pack of 25 count soft, pre-moistened Neutrogena Makeup Remover Cleansing Face Wipes to remove makeup and effectively cleanse skin in 1 easy step'
+  },
+
+  {
+    id: 5,
+    name: 'essence | Lash Princess False Lash Effect Mascara ',
+    img:"https://images-na.ssl-images-amazon.com/images/I/71vNRRHMLvL._SL1500_.jpg",
+    gal1:"https://images-na.ssl-images-amazon.com/images/I/71vNRRHMLvL._SL1500_.jpg",
+    gal2:"https://images-na.ssl-images-amazon.com/images/I/31cd26kbBLL.jpg",
+    gal3:"",
+    price: 12.09,
+    rating:4.4,
+    description: 'If you receive this mascara UNWRAPPED, it did not come from essence cosmetics.'
+  },
+
+  {
+    id: 6,
+    name: 'CeraVe Moisturizing Cream ',
+    img:"https://m.media-amazon.com/images/I/61S7BrCBj7L._AC_UL480_QL65_.jpg",
+    gal1:"https://m.media-amazon.com/images/I/61S7BrCBj7L._AC_UL480_QL65_.jpg",
+    gal2:"https://images-na.ssl-images-amazon.com/images/I/61lO1d7FSsL._SL1000_.jpg",
+    gal3:"https://images-na.ssl-images-amazon.com/images/I/61RzTGeRKRL._SL1000_.jpg",
+    price: 18.99,
+    rating:4.9,
+    description: 'With hyaluronic acid, ceramides and MVE technology for 24 hour hydration. Rich, velvety texture that leaves skin feeling smooth, it is absorbed quickly for softened skin without greasy, sticky, feel.'
+  },
+
+  {
+    id: 7,
+    name: 'TruSkin Vitamin C Serum for Face',
+    img:"https://m.media-amazon.com/images/I/516yN4iPdQL._AC_UL480_QL65_.jpg",
+    gal1:"https://m.media-amazon.com/images/I/516yN4iPdQL._AC_UL480_QL65_.jpg",
+    gal2:"https://images-na.ssl-images-amazon.com/images/I/71cDoRe7XkL._SL1500_.jpg",
+    gal3:"https://images-na.ssl-images-amazon.com/images/I/51qwH8YGR9L.jpg",
+    price: 29.99,
+    rating:4.0,
+    description: 'ANTIOXIDANT FACE SERUM - Vitamin C blends with Botanical Hyaluronic Acid, Vitamin E, Witch Hazel, and Jojoba Oil to promote your skins response to signs of aging like brightness, wrinkles, & dark spots'
+  },
+  {
+    id: 8,
+    name: 'Neutrogena Hydro Boost Hyaluronic Acid',
+    img:"https://m.media-amazon.com/images/I/71Bd6+BQQnL._AC_UL480_QL65_.jpg",
+    gal1:"https://m.media-amazon.com/images/I/71Bd6+BQQnL._AC_UL480_QL65_.jpg",
+    gal2:"https://images-na.ssl-images-amazon.com/images/I/81lnbAxwe2L._SL1500_.jpg",
+    gal3:"https://images-na.ssl-images-amazon.com/images/I/81ULS%2BLr8SL._SL1500_.jpg",
+    price: 16.97,
+    rating:4.5,
+    description: '1.7-fluid ounce jar of Neutrogena Hydro Boost hydrating water-gel face moisturizer with hyaluronic acid to hydrate dry skin'
+  },
+  {
+    id: 9,
+    name: 'Castor Oil (2oz) USDA Certified Organic',
+    img:"https://m.media-amazon.com/images/I/615AiiiHAUL._AC_UL480_QL65_.jpg",
+    gal1:"https://m.media-amazon.com/images/I/615AiiiHAUL._AC_UL480_QL65_.jpg",
+    gal2:"https://images-na.ssl-images-amazon.com/images/I/71Yo6DxPm0L._SL1500_.jpg",
+    gal3:"https://images-na.ssl-images-amazon.com/images/I/71lHRBvuRLL._SL1500_.jpg",
+    price: 9.99,
+    rating:4.67,
+    description: 'USDA CERTIFIED ORGANIC CASTOR OIL - Kate Blanc’s Organic Cold Pressed Castor Oil is certified by USDA and is guaranteed to be authentic, pure, natural, and hexane free. 100% Authentic or Full Refund.'
+  },
+  {
+    id: 10,
+    name: 'Weleda Skin Food Original Ultra-Rich Body Cream',
+    img:"https://m.media-amazon.com/images/I/71keoBqoiYL._AC_UL480_QL65_.jpg",
+    gal1:"https://m.media-amazon.com/images/I/71keoBqoiYL._AC_UL480_QL65_.jpg",
+    gal2:"https://images-na.ssl-images-amazon.com/images/I/81YAH1vge%2BL._SL1500_.jpg",
+    gal3:"https://images-na.ssl-images-amazon.com/images/I/71SHtxvUjbL._SL1500_.jpg",
+    price: 17.96,
+    rating:4.35,
+    description: 'Skin Food is a universal savior of dry, rough skin on faces, elbows, hands and feet.'
   }
 ];
 
